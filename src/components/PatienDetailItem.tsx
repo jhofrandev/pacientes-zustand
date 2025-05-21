719aa116324561758bb37e7a86b465a72b103ee7
Jhofran Briceño <jhofran.dev@outlook.com>
2025-05-21 14:47:23 -0500

    feat: implement PatientDetails and PatienDetailItem components for patient display

---
+++ src/components/PatienDetailItem.tsx
@@ -0,0 +1,16 @@
+type PatienDetailItemProps = {
+  label: string;
+  data: string;
+};
+
+export default function PatienDetailItem({
+  label,
+  data,
+}: PatienDetailItemProps) {
+  return (
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      {label}: {""}
+      <span className="font-normal normal-case">{data}</span>
+    </p>
+  );
+}
---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,35 @@
+import { Patient } from "../types";
+import PatienDetailItem from "./PatienDetailItem";
+
+type PatientDetailsProps = {
+  patient: Patient;
+};
+
+export default function PatientDetails({ patient }: PatientDetailsProps) {
+  return (
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+      <PatienDetailItem label="ID" data={patient.id} />
+      <PatienDetailItem label="Nombre" data={patient.name} />
+      <PatienDetailItem label="Propietario" data={patient.caretaker} />
+      <PatienDetailItem label="Email" data={patient.email} />
+      <PatienDetailItem label="Fecha Alta" data={patient.date.toString()} />
+      <PatienDetailItem label="Síntomas" data={patient.symptoms} />
+
+      <div className="flex justify-between gap-3 mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        >
+          Editar
+        </button>
+
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        >
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
+}
