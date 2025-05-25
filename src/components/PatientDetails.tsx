41de078c9249e9ce9212342079c2fcee70da5461
Jhofran Briceño <jhofran.dev@outlook.com>
2025-05-24 21:09:35 -0500

    feat: integrate Zustand store for patient management with delete and edit functionality

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,3 +1,4 @@
+import { usePatientStore } from "../store";
 import { Patient } from "../types";
 import PatienDetailItem from "./PatienDetailItem";
 
@@ -6,6 +7,8 @@ type PatientDetailsProps = {
 };
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
+  const { deletePatient, getPatientById } = usePatientStore();
+
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
       <PatienDetailItem label="ID" data={patient.id} />
@@ -15,10 +18,11 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
       <PatienDetailItem label="Fecha Alta" data={patient.date.toString()} />
       <PatienDetailItem label="Síntomas" data={patient.symptoms} />
 
-      <div className="flex justify-between gap-3 mt-10">
+      <div className="flex flex-col lg:flex-row justify-between gap-3 mt-10">
         <button
           type="button"
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => getPatientById(patient.id)}
         >
           Editar
         </button>
@@ -26,6 +30,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => deletePatient(patient.id)}
         >
           Eliminar
         </button>
--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,39 @@
 import { create } from "zustand";
 import { v4 as uuidv4 } from "uuid";
+import { devtools } from "zustand/middleware";
 import { DraftPatient, Patient } from "./types";
 
 type PatientState = {
   patients: Patient[];
+  activeId: Patient["id"];
   addPatient: (data: DraftPatient) => void;
+  deletePatient: (id: Patient["id"]) => void;
+  getPatientById: (id: Patient["id"]) => void;
 };
 
 const createPatient = (patien: DraftPatient): Patient => {
   return { ...patien, id: uuidv4() };
 };
 
-export const usePatientStore = create<PatientState>((set) => ({
-  patients: [],
-  addPatient: (data) => {
-    const newPatient = createPatient(data);
-    set((state) => ({
-      patients: [...state.patients, newPatient],
-    }));
-  },
-}));
+export const usePatientStore = create<PatientState>()(
+  devtools((set) => ({
+    patients: [],
+    activeId: "",
+    addPatient: (data) => {
+      const newPatient = createPatient(data);
+      set((state) => ({
+        patients: [...state.patients, newPatient],
+      }));
+    },
+    deletePatient: (id) => {
+      set((state) => ({
+        patients: state.patients.filter((patient) => patient.id !== id),
+      }));
+    },
+    getPatientById: (id) => {
+      set(() => ({
+        activeId: id,
+      }));
+    },
+  }))
+);
